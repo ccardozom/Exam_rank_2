7e2732b86c94b55f1e1d8e7f989f835a2d3f3714
Carlos alberto Cardozo medina <ccardozo@c2r17s2.42madrid.com>
2021-02-11 20:15:48 +0100

    version lowcost de GNL

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,107 +1,34 @@
 #include "get_next_line.h"
-#include <stdio.h>
-
-size_t	ft_strlen(char *s)
-{
-	size_t	len;
-
-	len = 0;
-	while (s[len])
-		len++;
-	return (len);
-}
-
-char 	*ft_strchr(char *s, int c)
-{
-	unsigned int	pos;
-
-	if (!s || !c)
-		return (NULL);
-	pos = 0;
-	while (s[pos])
-	{
-		if (s[pos] == c)
-			return (&s[pos]);
-		pos++;
-	}
-	return (NULL);
-}
-
-char	*ft_strjoin(char *s, char *p)
-{
-	int	pos;
-	char	*dest;
-
-	if (!(dest = malloc(ft_strlen(s) + ft_strlen(p) + 1)))
-		return (NULL);
-	pos = 0;
-	while (*s)
-		dest[pos++] = *s++;
-	while (*p)
-		dest[pos++] = *p++;
-	dest[pos] = '\0';
-	return (dest);
-}
-
-char	*ft_strdup(char *s)
-{
-	int		pos;
-	char	*dup;
-
-	if (!(dup = malloc(ft_strlen(s) + 1)))
-		return (NULL);
-	pos = 0;
-	while (s[pos])
-	{
-		dup[pos] = s[pos];
-		pos++;
-	}
-	dup[pos] = 0;
-	return (dup);
-}
 
 int		get_next_line(char **line)
 {
-	static char	*gnl;
-	char		buff[513];
-	char		*temp, *temp2;
-	int			bt;
-	if (!line)
+	int		bytes_buf;
+	char	buffer[1];
+	int		i;
+	char	*join;
+
+	if (!(*line = malloc(1)) || !line)
 		return (-1);
-	while (ft_strchr(gnl, '\n') == NULL)
+	(*line)[0] = '\0';
+	while ((bytes_buf = read(0, buffer, 1)) > 0)
 	{
-		bt = read(0, buff, 512);
-		if (bt < 0)
-			return (-1);
-		else if (!bt)
+		if (buffer[0] == '\0' || buffer[0] == '\n')
 			break ;
-		buff[bt] = 0;
-		if (!gnl)
-			gnl = ft_strdup(buff);
-		else
+		i = 0;
+		while ((*line)[i])
+			i++;
+		if (!(join = malloc(i + 2)))
+			return (-1);
+		i = 0;
+		while ((*line)[i] != '\0')
 		{
-			temp = ft_strjoin(gnl, buff);
-			free(gnl);
-			gnl = temp;
+			join[i] = (*line)[i];
+			i++;
 		}
+		join[i++] = buffer[0];
+		join[i++] = '\0';
+		free(*line);
+		*line = join;
 	}
-	if (!bt && !gnl)
-	{
-		*line = ft_strdup("");
-		return (0);
-	}
-	else if (!bt)
-	{
-		*line = ft_strdup("");
-		return (0);
-	}
-	if ((temp = ft_strchr(gnl, '\n')))
-	{
-		*temp = 0;
-		*line = ft_strdup(gnl);
-		temp2 = ft_strdup(++temp);
-		free(gnl);
-		gnl = temp2;
-	}
-	return (1);
+	return (bytes_buf);
 }
--- get_next_line/get_next_line_copy.c
+++ get_next_line/get_next_line_copy.c
@@ -1,15 +1,32 @@
 #include "get_next_line_copy.h"
-#include <stdio.h>
+
 int get_next_line(char **line)
 {
-    static char *gnl;
-    char buff[513], *temp, *temp2;
-    int bt;
+	char *join, buffer[1];
+	int ret, index;
 
-    if (!line)
-        return (-1);
-    while (ft_strchr(gnl, '\n') == NULL)
-    {
-
-    }
+	if (!(*line = malloc(1)) || !line)
+		return (-1);
+	(*line)[0] = '\0';
+	while ((ret = read(0, buffer, 1)) > 0)
+	{
+		if (buffer[0] == '\0' || buffer[0] == '\n')
+			break;
+		index = 0;
+		while ((*line)[index])
+			index++;
+		if (!(join = malloc(index + 2)))
+			return (-1);
+		index = 0;
+		while ((*line)[index] != '\0')
+		{
+			join[index] = (*line)[index];
+			index++;
+		}
+		join[index++] = buffer[0];
+		join[index++] = '\0';
+		free(*line);
+		*line = join;
+	}
+	return (ret);
 }
